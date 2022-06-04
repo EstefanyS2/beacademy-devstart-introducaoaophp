233a9a79aad108b86532c95772c2d68d93f4e26b
Estefany Alessandra <k.estefanyalessandra@gmail.com>
2022-06-04 14:00:31 -0300

    main

---
+++ modulo01/aula03/logicos.php
@@ -0,0 +1,19 @@
+<?php
+
+$numero =10;
+
+
+/*
+== Iquandade: Se um valor é igual a outro.
+=== Identicidade: So valor e o tipo são iguais.
+> maior que
+< menor que 
+>= maior igual a que
+<= menor igual a que.
+
+! negação
+!= negar a igualdade.
+!== negar a identicidade.
+ */
+
+echo $numero === 10; // === paramostrar se o numero é identico, se sim 1, se não 0.
