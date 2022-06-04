281f1d5a196cd74a8ad1d1f8b3794d295f548ed1
Estefany Alessandra <k.estefanyalessandra@gmail.com>
2022-06-04 14:05:01 -0300

    main

---
+++ modulo01/aula04/se-senao.php
@@ -0,0 +1,12 @@
+<?php  
+
+//estrutura de seleção composta de duas e apenas duas possibilidades, 1 ou 2.
+$n = 10;
+
+if ($n > 0) {
+    echo "Positivo";
+} else {
+    echo "Não é positivo";
+}
+
+echo "<br> Pronto, terminou";
