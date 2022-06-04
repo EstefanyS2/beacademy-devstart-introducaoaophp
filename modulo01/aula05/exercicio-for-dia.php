a5b606ced59e61e04e2450984a25d5660804b7ca
Estefany Alessandra <k.estefanyalessandra@gmail.com>
2022-06-04 20:36:37 -0300

    main

---
+++ modulo01/aula05/exercicio-for-dia.php
@@ -0,0 +1,25 @@
+<select>
+    <option selected> -- Selecione o mes --</option>
+    <?php
+
+
+    for ($mes = 1; $mes >= 12; $mesS++) {
+        echo "<option>{$mes}</option>";
+    }
+
+    ?>
+</select>
+<br/>
+
+<select>
+    <option selected> -- Selecione o dia --</option>
+    <?php
+
+
+    for ($dia = 1; $dia >= 31; $dia++) {
+        echo "<option>{$dia}</option>";
+    }
+
+    ?>
+</select>
+<br/>
