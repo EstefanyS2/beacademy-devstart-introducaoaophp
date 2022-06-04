4e20a47d83a7351c188fafb0e46d3613d76cf8d8
Estefany Alessandra <k.estefanyalessandra@gmail.com>
2022-06-04 14:03:35 -0300

    main

---
+++ modulo01/aula04/exercicio-se.php
@@ -0,0 +1,24 @@
+<?php
+
+if ($n > 0) {
+    echo "Positivo";
+} else {
+    if ($n < 0 ) {
+        echo "Negativo";
+    }
+    echo "Nulo";
+}
+
+
+if ($n > 0) {
+    echo "Positivo";
+}
+elseif ($n < 0) {
+    echo "Negativo";
+}
+else {
+    echo "Nulo";
+}
+
+
+echo "<br> Pronto, terminou";
