6d11033cb0b7a532560202234d36788e77359c3a
Estefany Alessandra <k.estefanyalessandra@gmail.com>
2022-06-06 18:25:21 -0300

    main

---
+++ modulo01/aula07/alunos.php
@@ -0,0 +1,26 @@
+<?php
+
+$a1 = [
+    'Chiquim',
+    'chiquim@email.com',
+    '85 9 8889-0000',
+    [
+        9,
+        8.9,
+        7.5,
+        6
+    ]
+];
+
+$a2 = [
+    'Maria',
+    'maria@email.com',
+    '85 9 8889-0000',
+];
+
+$alunos = [
+    $a1,
+    $a2,
+];
+
+var_dump($alunos);
