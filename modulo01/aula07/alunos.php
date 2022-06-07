4e376c916e048decf253bc6c5be14e514683bb2e
Estefany Alessandra <k.estefanyalessandra@gmail.com>
2022-06-07 17:32:39 -0300

    main

--- modulo01/aula07/alunos.php
+++ modulo01/aula07/alunos.php
@@ -1,10 +1,10 @@
 <?php
 
 $a1 = [
-    'Chiquim',
-    'chiquim@email.com',
-    '85 9 8889-0000',
-    [
+    'nome' => 'Andressa',
+    'email' => 'andressa@email.com',
+    'telefone' => '85 9 8889-0000',
+    'notas' => [
         9,
         8.9,
         7.5,
@@ -13,14 +13,73 @@ $a1 = [
 ];
 
 $a2 = [
-    'Maria',
-    'maria@email.com',
-    '85 9 8889-0000',
+    'nome' =>'Amanda',
+    'email' =>'amanda@email.com',
+    'telefone' => '85 9 8889-0000',
+    'notas' => [
+        9,
+        8.9,
+        7.5,
+        6
+    ]
+];
+
+$a3 = [
+    'nome' =>'Ester',
+    'email' =>'ester@email.com',
+    'telefone' => '85 9 8889-0000',
+    'notas' => [
+        9,
+        8.9,
+        7.5,
+        6
+    ]
+];
+
+$a4 = [
+    'nome' =>'Vanessa',
+    'email' =>'vanessa@email.com',
+    'telefone' => '85 9 8889-0000',
+    'notas' => [
+        9,
+        8.9,
+        7.5,
+        6
+    ]
 ];
 
 $alunos = [
     $a1,
     $a2,
+    $a3,
+    $a4,
 ];
 
-var_dump($alunos);
+?>
+
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<div class="container">
+    <h1 class="mt-5"> Alunos </h1>
+ <table class="table table-houver table-striped mt-5 ">
+    <thead class="table-dark">
+      <tr>
+          <th>Nome</th>
+          <th>Email</th>
+          <th>Telefone</th>
+      </tr>  
+    </thead>  
+    <tbody>
+         <?php
+         foreach($alunos as $cadaAluno) {
+            echo "<tr>"; 
+              echo '<td>' . $cadaAluno['nome'] .  "</td>";
+              echo '<td>' . $cadaAluno['email'] . "</td>";
+              echo '<td>' . $cadaAluno['telefone'] . "</td>";
+            echo '</tr>';
+        }
+
+        ?>
+    </tbody>
+</table>
+</div>
---
+++ modulo01/aula07/dados.php
@@ -0,0 +1,13 @@
+<?php
+//array associativo 
+$dados = [
+    'nome' => 'Chiguim',
+    'sobrenome' => 'Siva',
+    'cidade' => 'Fortaleza',
+    'idade' => 34,
+    'ano_formacao' => 2001,
+    'cor_preferida' => 'Azul',
+];
+
+echo 'Nome: ' . $dados['nome'] . '<br>';
+echo "Cidade: " . $dados['cidade'] . '<br>';
